f2c5a9684b69ddd4aef37c6ec84ca39d6c83b920
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2022-12-17 21:15:39 +0530

    Update after-install.sh

--- script/after-install.sh
+++ script/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "updating file permissions"
-chown -R ubuntu:ubuntu /home/ubuntu/goapp/
-chmod +x /home/ubuntu/goapp/main
+echo "Pulling image"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 554248189203.dkr.ecr.us-east-1.amazonaws.com
+docker pull 554248189203.dkr.ecr.us-east-1.amazonaws.com/goapp:demo
