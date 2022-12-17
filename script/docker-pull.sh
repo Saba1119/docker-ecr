f73cc5ab87b90944f4bee42505029e1494ff0467
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2022-12-17 21:16:16 +0530

    Delete docker-pull.sh

--- script/docker-pull.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Pulling image"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 554248189203.dkr.ecr.us-east-1.amazonaws.com
-docker pull 554248189203.dkr.ecr.us-east-1.amazonaws.com/goapp:demo
