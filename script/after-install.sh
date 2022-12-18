28ebf232ee48bf8f522324701ebe3506e31c63ef
saba <sabanatarajan89@gmail.com>
2022-12-18 17:37:24 +0530

    Updated Changes

--- script/after-install.sh
+++ script/after-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "updating file permissions"
-chown -R ubuntu:ubuntu /home/ubuntu/goapp/
-chmod +x -R /home/ubuntu/goapp/
+chown -R ubuntu:ubuntu /home/ubuntu/docker-ecr/
+chmod +x -R /home/ubuntu/docker-ecr/
 
 echo "Pulling image"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 554248189203.dkr.ecr.us-east-1.amazonaws.com
