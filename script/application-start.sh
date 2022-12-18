5af7fa49ea345b94bdcf3d0ec40cb02fa840e624
saba <sabanatarajan89@gmail.com>
2022-12-18 16:55:05 +0530

    Updated new changes

--- script/application-start.sh
+++ script/application-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "starting Docker"
-docker run --name goapp 554248189203.dkr.ecr.us-east-1.amazonaws.com/goapp:demo
+
+docker run -d --name goapp -p 8081:8081 554248189203.dkr.ecr.us-east-1.amazonaws.com/goapp:demo
