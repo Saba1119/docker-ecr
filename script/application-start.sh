fcedcedca30b2c33cf39597ae3b03bdfe254338a
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2022-12-20 10:10:22 +0530

    Update application-start.sh

--- script/application-start.sh
+++ script/application-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "starting Docker"
 
-docker run -d --name demo-faragte-goapp -p 8081:8081 554248189203.dkr.ecr.us-east-1.amazonaws.com/goapp:demo
+docker run -d --goapp -p 8081:8081 554248189203.dkr.ecr.us-east-1.amazonaws.com/goapp:demo
