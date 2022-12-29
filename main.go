ddfcb5862a76a0dfe4196dd1027e1b78fafec0f6
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2022-12-29 17:45:16 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hi hello. %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "hi . %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
