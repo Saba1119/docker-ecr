3d527decd3116fa2da53683361699c6cf10f5562
Gokul-git4 <119651989+Gokul-git4@users.noreply.github.com>
2022-12-30 17:12:26 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hi from aws . %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "hi from github . %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
