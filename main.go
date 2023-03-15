d454f102375c8b06c47606301ac438f8256974c7
Gokul-git4 <119651989+Gokul-git4@users.noreply.github.com>
2023-03-15 18:59:08 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,16 +2,95 @@ package main
 
 import (
 	"fmt"
-	"html"
+	"image/color"
+	"image/png"
 	"log"
-	"net/http"
+	"os"
 )
 
+type ImageSet interface {
+	Set(x, y int, c color.Color)
+}
+
 func main() {
+	file, err := os.Open("./images/selfcss.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hi from AWS using service catalog using pipelines %q", html.EscapeString(r.URL.Path))
-	})
+	img, err := png.Decode(file)
+	if err != nil {
+		log.Fatal(os.Stderr, "%s: %v\n", "./images/selfcss.png", err)
+	}
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	b := img.Bounds()
+
+	imgSet := img.(ImageSet)
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			oldPixel := img.At(x, y)
+			r, g, b, a := oldPixel.RGBA()
+			fmt.Println(r, g, b, a)
+			pixel := color.RGBA{uint8(g), uint8(g), uint8(g), uint8(a)}
+			imgSet.Set(x, y, pixel)
+		}
+	}
+
+	fd, err := os.Create("./images/gray.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = png.Encode(fd, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = fd.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err = os.Open("./images/selfcss.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	img, err = png.Decode(file)
+	if err != nil {
+		log.Fatal(os.Stderr, "%s: %v\n", "./images/selfcss.png", err)
+	}
+
+	b = img.Bounds()
+
+	imgSet = img.(ImageSet)
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			oldPixel := img.At(x, y)
+			r, g, b, a := oldPixel.RGBA()
+			//fmt.Println(r, g, b, a)
+			r = 65535 - r
+			g = 65535 - g
+			b = 65535 - b
+			pixel := color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
+			imgSet.Set(x, y, pixel)
+		}
+	}
+
+	fd, err = os.Create("./images/inv.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = png.Encode(fd, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = fd.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
