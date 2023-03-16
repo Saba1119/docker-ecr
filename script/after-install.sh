2d5793772bc996d24b8894e48787cc11979e9c73
Saba1119 <100400271+Saba1119@users.noreply.github.com>
2023-03-16 17:56:52 +0530

    Delete after-install.sh

--- script/after-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "updating file permissions"
-chown -R ubuntu:ubuntu /home/ubuntu/docker-ecr/
-chmod +x -R /home/ubuntu/docker-ecr/
-
-echo "Pulling image"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 554248189203.dkr.ecr.us-east-1.amazonaws.com
-docker pull 554248189203.dkr.ecr.us-east-1.amazonaws.com/goapp:demo
